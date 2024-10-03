a593232a3b51f053e46188c3021d5bb7f541ba43
Calvin <calvingsx@gmail.com>
2024-10-03 11:07:29 -0300

    Fix: Ajustando texto de boas vindas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <div>
-      <main></main>
+      <main>
+        <div className="text-primary rounded-md px-4 py-4 space-y-2">
+          <h1 className="text-2xl">
+            Bem vindo, <span className="font-semibold">Calvin</span>
+          </h1>          
+        </div>
+      </main>
     </div>
   );
 }
