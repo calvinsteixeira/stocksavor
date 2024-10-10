a0a28704f8b81e9bcf1ef5927e0ab6d4ccb755be
Calvin <calvingsx@gmail.com>
2024-10-10 15:23:45 -0300

    Feat: Adicionando skeletons

--- app/stock/product/page.tsx
+++ app/stock/product/page.tsx
@@ -6,6 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import * as Icons from '@/icons';
+import { Skeleton } from '@/components/ui/skeleton';
 
 //UTILS
 import React from 'react';
@@ -93,89 +94,104 @@ export default function page(props: Props) {
         <Icons.ArrowLeft className="absolute left-4 t-0 text-primary" onClick={() => router.push('/')} />
         <h2 className="text-primary w-full text-2xl font-semibold">Detalhes do item</h2>
         <div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(updateProduct)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome</FormLabel>
-                    <FormControl>
-                      <Input readOnly={!allowEdit} disabled={!allowEdit} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Descrição</FormLabel>
-                    <FormControl>
-                      <Textarea readOnly={!allowEdit} disabled={!allowEdit} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+          {!productData ? (
+            <div className="space-y-4">
+              <Skeleton className="h-12 w-full"/>
+              <Skeleton className="h-24 w-full"/>
               <div className="flex gap-4">
-                <FormField
-                  control={form.control}
-                  name="expirationDate"
-                  render={({ field }) => (
-                    <FormItem className="w-[60%]">
-                      <FormLabel>Data de validade</FormLabel>
-                      <FormControl>
-                        <Input readOnly={!allowEdit} disabled={!allowEdit} type="date" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="amount"
-                  render={({ field }) => (
-                    <FormItem className="flex-1">
-                      <FormLabel>Quantidade</FormLabel>
-                      <FormControl>
-                        <Input readOnly={!allowEdit} disabled={!allowEdit} type="number" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                <Skeleton className="h-12 w-[60%]"/>
+                <Skeleton className="h-12 flex-1"/>
               </div>
-              <div className="w-full space-x-2">
-                <Button
-                  variant={'secondary'}
-                  onClick={() => {
-                    setAllowEdit(!allowEdit);
-                    if (allowEdit) {
-                      if (productData) {
-                        form.reset(productData);
-                      } else {
-                        form.reset();
+              <div className="flex gap-4">
+                <Skeleton className="h-12 w-28"/>
+                <Skeleton className="h-12 w-28"/>
+              </div>
+            </div>
+          ) : (
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(updateProduct)} className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nome</FormLabel>
+                      <FormControl>
+                        <Input readOnly={!allowEdit} disabled={!allowEdit} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Descrição</FormLabel>
+                      <FormControl>
+                        <Textarea readOnly={!allowEdit} disabled={!allowEdit} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex gap-4">
+                  <FormField
+                    control={form.control}
+                    name="expirationDate"
+                    render={({ field }) => (
+                      <FormItem className="w-[60%]">
+                        <FormLabel>Data de validade</FormLabel>
+                        <FormControl>
+                          <Input readOnly={!allowEdit} disabled={!allowEdit} type="date" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="amount"
+                    render={({ field }) => (
+                      <FormItem className="flex-1">
+                        <FormLabel>Quantidade</FormLabel>
+                        <FormControl>
+                          <Input readOnly={!allowEdit} disabled={!allowEdit} type="number" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="w-full space-x-2">
+                  <Button
+                    variant={'secondary'}
+                    onClick={() => {
+                      setAllowEdit(!allowEdit);
+                      if (allowEdit) {
+                        if (productData) {
+                          form.reset(productData);
+                        } else {
+                          form.reset();
+                        }
                       }
-                    }
-                  }}
-                  type="button"
-                  disabled={loading}
-                >
-                  {allowEdit ? 'Cancelar edição' : 'Editar informações'}
-                </Button>
-                {(allowEdit || loading) && (
-                  <Button disabled={(!allowEdit && !allowSave) || loading} type="submit">
-                    {loading && <Icons.Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-                    {loading ? 'Salvando' : 'Salvar'}
+                    }}
+                    type="button"
+                    disabled={loading}
+                  >
+                    {allowEdit ? 'Cancelar edição' : 'Editar informações'}
                   </Button>
-                )}
-              </div>
-            </form>
-          </Form>
+                  {(allowEdit || loading) && (
+                    <Button disabled={(!allowEdit && !allowSave) || loading} type="submit">
+                      {loading && <Icons.Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                      {loading ? 'Salvando' : 'Salvar'}
+                    </Button>
+                  )}
+                </div>
+              </form>
+            </Form>
+          )}
         </div>
       </main>
     </div>
